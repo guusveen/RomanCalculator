b6035443189cae323712581f440b8fa9dc18c196
goezie <goezie@protonmail.com>
2020-08-21 14:53:02 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace RomanCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BI = new System.Windows.Forms.Button();
+            this.BX = new System.Windows.Forms.Button();
+            this.BL = new System.Windows.Forms.Button();
+            this.BC = new System.Windows.Forms.Button();
+            this.BD = new System.Windows.Forms.Button();
+            this.BM = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BV = new System.Windows.Forms.Button();
+            this.BCalculate = new System.Windows.Forms.Button();
+            this.BDivide = new System.Windows.Forms.Button();
+            this.BMultiply = new System.Windows.Forms.Button();
+            this.BPlus = new System.Windows.Forms.Button();
+            this.BMinus = new System.Windows.Forms.Button();
+            this.BDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BI
+            // 
+            this.BI.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BI.Location = new System.Drawing.Point(12, 118);
+            this.BI.Name = "BI";
+            this.BI.Size = new System.Drawing.Size(50, 28);
+            this.BI.TabIndex = 0;
+            this.BI.Text = "I";
+            this.BI.UseVisualStyleBackColor = true;
+            this.BI.Click += new System.EventHandler(this.BI_Click);
+            // 
+            // BX
+            // 
+            this.BX.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BX.Location = new System.Drawing.Point(124, 118);
+            this.BX.Name = "BX";
+            this.BX.Size = new System.Drawing.Size(50, 28);
+            this.BX.TabIndex = 2;
+            this.BX.Text = "X";
+            this.BX.UseVisualStyleBackColor = true;
+            this.BX.Click += new System.EventHandler(this.BX_Click);
+            // 
+            // BL
+            // 
+            this.BL.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BL.Location = new System.Drawing.Point(12, 152);
+            this.BL.Name = "BL";
+            this.BL.Size = new System.Drawing.Size(50, 28);
+            this.BL.TabIndex = 3;
+            this.BL.Text = "L";
+            this.BL.UseVisualStyleBackColor = true;
+            this.BL.Click += new System.EventHandler(this.BL_Click);
+            // 
+            // BC
+            // 
+            this.BC.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BC.Location = new System.Drawing.Point(68, 152);
+            this.BC.Name = "BC";
+            this.BC.Size = new System.Drawing.Size(50, 28);
+            this.BC.TabIndex = 4;
+            this.BC.Text = "C";
+            this.BC.UseVisualStyleBackColor = true;
+            this.BC.Click += new System.EventHandler(this.BC_Click);
+            // 
+            // BD
+            // 
+            this.BD.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BD.Location = new System.Drawing.Point(124, 152);
+            this.BD.Name = "BD";
+            this.BD.Size = new System.Drawing.Size(50, 28);
+            this.BD.TabIndex = 5;
+            this.BD.Text = "D";
+            this.BD.UseVisualStyleBackColor = true;
+            this.BD.Click += new System.EventHandler(this.BD_Click);
+            // 
+            // BM
+            // 
+            this.BM.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BM.Location = new System.Drawing.Point(68, 186);
+            this.BM.Name = "BM";
+            this.BM.Size = new System.Drawing.Size(50, 28);
+            this.BM.TabIndex = 6;
+            this.BM.Text = "M";
+            this.BM.UseVisualStyleBackColor = true;
+            this.BM.Click += new System.EventHandler(this.BM_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 19);
+            this.label1.TabIndex = 7;
+            // 
+            // BV
+            // 
+            this.BV.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BV.Location = new System.Drawing.Point(68, 118);
+            this.BV.Name = "BV";
+            this.BV.Size = new System.Drawing.Size(50, 28);
+            this.BV.TabIndex = 1;
+            this.BV.Text = "V";
+            this.BV.UseVisualStyleBackColor = true;
+            this.BV.Click += new System.EventHandler(this.BV_Click);
+            // 
+            // BCalculate
+            // 
+            this.BCalculate.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BCalculate.Location = new System.Drawing.Point(197, 118);
+            this.BCalculate.Name = "BCalculate";
+            this.BCalculate.Size = new System.Drawing.Size(50, 28);
+            this.BCalculate.TabIndex = 8;
+            this.BCalculate.Text = "=";
+            this.BCalculate.UseVisualStyleBackColor = true;
+            this.BCalculate.Click += new System.EventHandler(this.BCalculate_Click);
+            // 
+            // BDivide
+            // 
+            this.BDivide.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BDivide.Location = new System.Drawing.Point(253, 186);
+            this.BDivide.Name = "BDivide";
+            this.BDivide.Size = new System.Drawing.Size(50, 28);
+            this.BDivide.TabIndex = 9;
+            this.BDivide.Text = "/";
+            this.BDivide.UseVisualStyleBackColor = true;
+            this.BDivide.Click += new System.EventHandler(this.BDivide_Click);
+            // 
+            // BMultiply
+            // 
+            this.BMultiply.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BMultiply.Location = new System.Drawing.Point(253, 152);
+            this.BMultiply.Name = "BMultiply";
+            this.BMultiply.Size = new System.Drawing.Size(50, 28);
+            this.BMultiply.TabIndex = 10;
+            this.BMultiply.Text = "*";
+            this.BMultiply.UseVisualStyleBackColor = true;
+            this.BMultiply.Click += new System.EventHandler(this.BMultiply_Click);
+            // 
+            // BPlus
+            // 
+            this.BPlus.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BPlus.Location = new System.Drawing.Point(197, 152);
+            this.BPlus.Name = "BPlus";
+            this.BPlus.Size = new System.Drawing.Size(50, 28);
+            this.BPlus.TabIndex = 11;
+            this.BPlus.Text = "+";
+            this.BPlus.UseVisualStyleBackColor = true;
+            this.BPlus.Click += new System.EventHandler(this.BPlus_Click);
+            // 
+            // BMinus
+            // 
+            this.BMinus.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BMinus.Location = new System.Drawing.Point(197, 186);
+            this.BMinus.Name = "BMinus";
+            this.BMinus.Size = new System.Drawing.Size(50, 28);
+            this.BMinus.TabIndex = 12;
+            this.BMinus.Text = "-";
+            this.BMinus.UseVisualStyleBackColor = true;
+            this.BMinus.Click += new System.EventHandler(this.BMinus_Click);
+            // 
+            // BDelete
+            // 
+            this.BDelete.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BDelete.Location = new System.Drawing.Point(253, 118);
+            this.BDelete.Name = "BDelete";
+            this.BDelete.Size = new System.Drawing.Size(50, 28);
+            this.BDelete.TabIndex = 13;
+            this.BDelete.Text = "del";
+            this.BDelete.UseVisualStyleBackColor = true;
+            this.BDelete.Click += new System.EventHandler(this.BDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 233);
+            this.Controls.Add(this.BDelete);
+            this.Controls.Add(this.BMinus);
+            this.Controls.Add(this.BPlus);
+            this.Controls.Add(this.BMultiply);
+            this.Controls.Add(this.BDivide);
+            this.Controls.Add(this.BCalculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BM);
+            this.Controls.Add(this.BD);
+            this.Controls.Add(this.BC);
+            this.Controls.Add(this.BL);
+            this.Controls.Add(this.BX);
+            this.Controls.Add(this.BV);
+            this.Controls.Add(this.BI);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BI;
+        private System.Windows.Forms.Button BX;
+        private System.Windows.Forms.Button BL;
+        private System.Windows.Forms.Button BC;
+        private System.Windows.Forms.Button BD;
+        private System.Windows.Forms.Button BM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BV;
+        private System.Windows.Forms.Button BCalculate;
+        private System.Windows.Forms.Button BDivide;
+        private System.Windows.Forms.Button BMultiply;
+        private System.Windows.Forms.Button BPlus;
+        private System.Windows.Forms.Button BMinus;
+        private System.Windows.Forms.Button BDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomanCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int currentResult = 0;
+        string currentRoman = "";
+        string firstRoman = "";
+        int firstNumber = 0;
+        string secondRoman = "";
+        int secondNumber = 0;
+        string operation = "";
+        int result = 0;
+        string resultRoman = "";
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BM_Click(object sender, EventArgs e)
+        {
+            if(currentResult % 1000 == 0 && currentResult <= 2000 || currentResult == 0)
+            {
+                currentResult += 1000;
+                currentRoman += "M";
+                label1.Text = currentRoman;
+            }
+        }
+
+        private void BD_Click(object sender, EventArgs e)
+        {
+            if(currentResult % 1000 == 0 || currentResult == 0 )
+            {
+                currentResult += 500;
+                currentRoman += "D";
+                label1.Text = currentRoman;
+            }
+            if (currentResult % 1000 == 100)
+            {
+                currentResult += 300;
+                currentRoman += "D";
+                label1.Text = currentRoman;
+
+            }
+        }
+
+        private void BC_Click(object sender, EventArgs e)
+        {
+            if( currentResult == 0 ||
+                currentResult % 500 == 0 ||
+                currentResult % 500 == 100 ||
+                currentResult % 500 == 200)
+            {
+                currentResult += 100;
+                currentRoman += "C";
+                label1.Text = currentRoman;
+            }
+            if( currentResult % 100 == 10)
+            {
+                currentResult += 80;
+                currentRoman += "C";
+                label1.Text = currentRoman;
+            }
+        }
+
+        private void BL_Click(object sender, EventArgs e)
+        {
+            if (currentResult % 100 == 0 || currentResult == 0)
+            {
+                currentResult += 50;
+                currentRoman += "L";
+                label1.Text = currentRoman;
+            }
+            if (currentResult % 100 == 10)
+            {
+                currentResult += 30;
+                currentRoman += "L";
+                label1.Text = currentRoman;
+            }
+        }
+
+        private void BX_Click(object sender, EventArgs e)
+        {
+            if (currentResult == 0 ||
+                currentResult % 50 == 0 ||
+                currentResult % 50 == 10 ||
+                currentResult % 50 == 20)
+            {
+                currentResult += 10;
+                currentRoman += "X";
+                label1.Text = currentRoman;
+            }
+            if (currentResult % 10 == 1)
+            {
+                currentResult += 8;
+                currentRoman += "X";
+                label1.Text = currentRoman;
+            }
+        }
+
+        private void BV_Click(object sender, EventArgs e)
+        {
+            if (currentResult % 10 == 0 || currentResult == 0)
+            {
+                currentResult += 5;
+                currentRoman += "V";
+                label1.Text = currentRoman;
+            }
+            if (currentResult % 10 == 1)
+            {
+                currentResult += 3;
+                currentRoman += "V";
+                label1.Text = currentRoman;
+            }
+        }
+
+        private void BI_Click(object sender, EventArgs e)
+        {
+          if(currentResult % 5 < 3 || currentResult == 0)
+            {
+                currentRoman += "I";
+                currentResult += 1;
+                label1.Text = currentRoman;
+            }
+            MessageBox.Show($"{firstNumber} {secondNumber} ");
+        }
+        
+        private void BPlus_Click(object sender, EventArgs e)
+        {
+            firstRoman = currentRoman;
+            firstNumber = currentResult;
+            currentRoman = "";
+            currentResult = 0;
+            label1.Text = currentRoman;
+            operation = "+";
+        }
+
+        private void BMinus_Click(object sender, EventArgs e)
+        {
+            firstRoman = currentRoman;
+            firstNumber = currentResult;
+            currentRoman = "";
+            currentResult = 0;
+            operation = "-";
+        }
+
+        private void BMultiply_Click(object sender, EventArgs e)
+        {
+            firstRoman = currentRoman;
+            firstNumber = currentResult;
+            currentRoman = "";
+            currentResult = 0;
+            operation = "*";
+        }
+
+        private void BDivide_Click(object sender, EventArgs e)
+        {
+            firstRoman = currentRoman;
+            firstNumber = currentResult;
+            currentRoman = "";
+            currentResult = 0;
+            operation = "/";
+        }
+
+        private void BCalculate_Click(object sender, EventArgs e)
+        {
+            secondNumber = currentResult;
+            if (operation == "+")
+            {
+                result = firstNumber + secondNumber;
+                resultRoman = ToRoman(result);
+                label1.Text = resultRoman;
+            }
+            if (operation == "-")
+            {
+                result = firstNumber - secondNumber;
+                resultRoman = ToRoman(result);
+                label1.Text = resultRoman;
+            }
+            if (operation == "*")
+            {
+                result = firstNumber * secondNumber;
+                resultRoman = ToRoman(result);
+                label1.Text = resultRoman;
+            }
+            if (operation == "/")
+            {
+                result = firstNumber / secondNumber;
+                resultRoman = ToRoman(result);
+                label1.Text = resultRoman;
+            }
+            secondNumber = 0;
+            secondRoman = "";
+            firstNumber = result;
+            firstRoman = ToRoman(result);
+            operation = "";
+        }
+
+        private void BDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string ToRoman(int number)
+        {
+            if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("insert value betwheen 1 and 3999");
+            if (number < 1) return string.Empty;
+            if (number >= 1000) return "M" + ToRoman(number - 1000);
+            if (number >= 900) return "CM" + ToRoman(number - 900);
+            if (number >= 500) return "D" + ToRoman(number - 500);
+            if (number >= 400) return "CD" + ToRoman(number - 400);
+            if (number >= 100) return "C" + ToRoman(number - 100);
+            if (number >= 90) return "XC" + ToRoman(number - 90);
+            if (number >= 50) return "L" + ToRoman(number - 50);
+            if (number >= 40) return "XL" + ToRoman(number - 40);
+            if (number >= 10) return "X" + ToRoman(number - 10);
+            if (number >= 9) return "IX" + ToRoman(number - 9);
+            if (number >= 5) return "V" + ToRoman(number - 5);
+            if (number >= 4) return "IV" + ToRoman(number - 4);
+            if (number >= 1) return "I" + ToRoman(number - 1);
+            throw new ArgumentOutOfRangeException("something bad happened");
+        }
+    }
+}
